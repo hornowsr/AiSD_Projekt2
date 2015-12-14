66413031b6cd57445c82c02d25e855365ca38e26
kuzniarg <kuzniarg@ee.pw.edu.pl>
2015-12-14 19:46:18 +0100

    Pierwszy commit

---
+++ AiSD_Projekt2/src/aisd_projekt2/AiSD_Projekt2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aisd_projekt2;
+
+/**
+ *
+ * @author kuzniarg
+ */
+public class AiSD_Projekt2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
